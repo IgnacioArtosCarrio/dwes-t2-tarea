cf115e967f9fd9a700ff4914be20143156efd818
Ignacio Artos Carrio <ignacioartosdeveloper@gmail.com>
2025-02-01 21:06:50 +0100

    mejora del ejercicio triangulo

--- clases/TriangleGenerator.php
+++ clases/TriangleGenerator.php
@@ -5,13 +5,17 @@ class TriangleGenerator {
     public static function generateTriangle(int $altura): string {
         if ($altura < 0) return "";
 
+        $output = "<pre style='font-family: monospace; white-space: pre;'>\n";
 
-        $output = "<pre style='font-family: monospace; text-align: center; white-space: pre;'>\n";
+        $maxWidth = ($altura * 2) - 1;
 
         for ($i = 0; $i < $altura; $i++) {
-            $espacios = str_repeat(" ", $altura - $i - 1);
-            $estrellas = str_repeat("*", $i * 2 + 1);
-            $output .= "$espacios$estrellas\n";
+            $estrellas = str_repeat("*", ($i * 2) + 1);
+            $totalEspacios = $maxWidth - strlen($estrellas);
+            $espaciosIzquierda = intdiv($totalEspacios, 2);
+            $espaciosDerecha = $totalEspacios - $espaciosIzquierda;
+
+            $output .= str_repeat(" ", $espaciosIzquierda) . $estrellas . str_repeat(" ", $espaciosDerecha) . "\n";
         }
 
         $output .= "</pre>";
